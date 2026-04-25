5d7cfee1501b699f8962c991c1d57fa063f91625
Hairum Qureshi <hqureshi@udel.edu>
2026-04-25 00:13:02 -0400

    installed React + TailwindCSS

---
+++ react-its/src/components/App.tsx
@@ -0,0 +1,9 @@
+import "../css/index.css"; // this assumes you have the index.css file in a 'css' folder
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
