2e144ffb61efe087b645a325781e3bf6db5f9019
Hairum Qureshi <hqureshi@udel.edu>
2026-05-14 02:14:17 -0400

    created ITS type

--- react-its/src/types.ts
+++ react-its/src/types.ts
@@ -1,7 +1,17 @@
 type ParsedComponent = {
+  name: string;
   id: string;
   parent: string | null;
   props: Record<string, string>;
 };
 
-export type { ParsedComponent };
+type ITS = {
+  id: string;
+  text: string;
+  answer: string;
+  correctMessage: string;
+  incorrectMessage: string;
+  showInput: boolean; 
+};
+
+export type { ParsedComponent, ITS };
